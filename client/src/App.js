56d1c5b7f0eb0cfc967ffdaa29c6a66d2cc689b1
Pratik Mandloi <96142306+pratikmandloi@users.noreply.github.com>
2023-12-01 13:56:54 +0530

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,18 @@ import { Outlet } from 'react-router-dom';
 import { Footer } from './Components/Footer/Footer';
 import { Navigation1 } from './Components/Navigation/Navigation1';
 import { Navigation2 } from './Components/Navigation/Navigation2';
-
+import ErrorBoundaries from "./Components/ErrorBoundaries/ErrorBoundaries"
 function App() {
 
   return (
-    <>
+    <ErrorBoundaries>
     <div className='main-app'>
       {/* <Navigation2/> */}
       <Navigation1/>
       <Outlet />
       <Footer />
     </div>
-    </>
+    </ErrorBoundaries>
   );
 }
 
