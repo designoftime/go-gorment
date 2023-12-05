86c77d617941933866de50631114e99b22fca4ce
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2023-12-05 18:57:06 +0530

    Added Privacy Policies

--- client/src/Components/PrivacyPolicy/Privacypolicy.jsx
+++ client/src/Components/PrivacyPolicy/Privacypolicy.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import Container from '../../../../admin/src/components/image-uploader/Container'
+import { useSelector } from 'react-redux'
 export const Privacypolicy = () => {
+
+  const privacyPolicyData = useSelector(store => store?.storeSettings?.privacy_policy);
+
+  console.log(privacyPolicyData);
+
   return (
     <div className='Container'>
-        
+        <h1 className='m-5 p-5'> Privacy Policy </h1>
+        {privacyPolicyData?.description?.en}
     </div>
   )
 }
