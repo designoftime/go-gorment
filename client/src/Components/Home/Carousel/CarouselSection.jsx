6e125847473a5049590c5586b485da43683536f1
Pratik Mandloi <96142306+pratikmandloi@users.noreply.github.com>
2023-12-08 11:33:49 +0530

    Fix Frontend

--- client/src/Components/Home/Carousel/CarouselSection.jsx
+++ client/src/Components/Home/Carousel/CarouselSection.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { React, useRef } from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import './Carousel.css';
 import 'swiper/css';
 import demo from '../../../assets/demo.png';
@@ -11,34 +11,41 @@ import { useState } from 'react';
 
 export const CarouselSection = () => {
 
-    const [activeSlider, setActiveSlider] =  useState(0);
-    const allImage = [img1,img2,img3,img4]
+    const first = useRef();
+
+
+    const [activeSlider, setActiveSlider] = useState(0);
+
+    const allImage = [img1, img2, img3, img4]
     return (
         <>
             <div className="main-swiper-carousel">
                 <Swiper
                     className='swiper'
+                    ref={first}
                     spaceBetween={0}
                     slidesPerView={1}
                     onSlideChange={(e) => {
                         setActiveSlider(e.activeIndex)
+                        console.log(e);
                     }}
-                    // onSwiper={(swiper) => console.log(swiper)}
+                    // onSwiper={(swiper) => swiper.slideTo}
                 >
                     <SwiperSlide className='slide'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque,</SwiperSlide>
 
-                    <SwiperSlide className='slide'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque, consequatur corporis </SwiperSlide>
+                    <SwiperSlide className='slide'> sit, amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque, consequatur corporis </SwiperSlide>
 
-                    <SwiperSlide className='slide'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque, consequatur corporis </SwiperSlide>
+                    <SwiperSlide className='slide'>ipsum dolor sit, amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque, consequatur corporis </SwiperSlide>
 
-                    <SwiperSlide className='slide'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque, consequatur corporis </SwiperSlide>
+                    <SwiperSlide className='slide'>amet consectetur adipisicing elit. Nulla laudantium ut nostrum atque, consequatur corporis </SwiperSlide>
                 </Swiper>
 
                 <div className="slide-logos mt-4">
-
                     {
                         allImage.map((img, idx) => {
-                            return (<img key={idx} src={img} alt="" className={idx==activeSlider ? "active-logo" : null}/>)
+                            return (<img key={idx} src={img} alt="" className={idx == activeSlider ? "active-logo" : null} onClick={(e) => {
+                                setActiveSlider(idx)
+                            }} />)
                         })
                     }
                 </div>
