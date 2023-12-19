40731ce5d7bbf56c491b16c3a92008c840909ad5
nikhilsagar3108 <64469377+nikhilsagar3108@users.noreply.github.com>
2023-12-19 19:35:47 +0530

    new hook and maintained some responsive in home

--- client/src/Components/Home/CustomerCaursel/CustomerCaursel.jsx
+++ client/src/Components/Home/CustomerCaursel/CustomerCaursel.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import './CustomerCaursel.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-
+import { Navigation, Pagination } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { sliderVal } from '../../../utils/Constants'
+import useWindowInnerWidth from '../../hooks/useWindowInnerWidth';
 
 export const CustomerCaursel = ({ bottomSliderData }) => {
+    const showValue = useWindowInnerWidth();
 
     // if (!bottomSliderData) {
     //     return;
     // }
-
     const [toggleAni, setToggleAni] = useState(false);
     const [backgroundStyle, setBackgroundStyle] = useState({
         fullBg: {
@@ -60,13 +60,16 @@ export const CustomerCaursel = ({ bottomSliderData }) => {
                                 <Swiper
                                     spaceBetween={50}
                                     slidesPerView={1}
-                                    navigation={true}
+                                    navigation={showValue < 767 ? false : true}
                                     onSlideChange={(e) => {
                                         setToggleAni(!toggleAni)
                                         handleStyleChange(e.activeIndex);
                                     }}
+                                    pagination={{
+                                        dynamicBullets:true,
+                                    }}
                                     className='customer-slider'
-                                    modules={[Navigation]}
+                                    modules={ showValue < 767 ?[Pagination] : [Navigation]}
                                 >
                                     {
                                         sliderVal.map((val) => {
---
+++ client/src/Components/hooks/useWindowInnerWidth.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+const useWindowInnerWidth = () => {
+    const [widthValue,setWidthValue] = useState(window.innerWidth);
+    useEffect(()=>{
+        const handleWidth =()=>{
+            setWidthValue(window.innerWidth);
+        };
+        window.addEventListener('resize',handleWidth);
+        return()=>{
+
+            window.removeEventListener('resize',handleWidth);
+        }
+    })
+  return widthValue;
+}
+export default useWindowInnerWidth;
