91d67b691b4276e26d2fd5073083b84a1eeda626
nikhilsagar3108 <64469377+nikhilsagar3108@users.noreply.github.com>
2023-12-25 16:08:13 +0530

    Responsive remaining in card

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import { CgArrowLongLeft } from "react-icons/cg";
-import { Button } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 import OffCanvas from "react-bootstrap/Offcanvas";
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { HiOutlineTrash } from "react-icons/hi";
 import { AiOutlinePlusSquare, AiOutlineMinusSquare } from "react-icons/ai";
+import Backarrow from './images/back.svg'
 import "./Cart.css";
 import "../Accounts/Ollys-Login/Login.css"
 import "../Navigation/Navigation1.css";
 import "../Navigation/Navigation2.css";
 import cartProductImg1 from "../Shop/images/SourCreamHoverimg.jpg";
 import cartProductImg2 from "../Shop/images/DarkChoc-Case_360x.png";
+import Star from './images/star.svg'
 export const Cart = () => {
     const [productNo, setProductNo] = useState(0)
     const [showcart, setShowCart] = useState(false);
@@ -27,20 +29,7 @@ export const Cart = () => {
             cartpsize: "REGULAR (10 X 35G)",
             cartpprice: "£50.00",
         },
-        {
-            id: 2,
-            cartpimage: cartProductImg2,
-            cartpheader: "Dark Choclate PRETZEL THINS",
-            cartpsize: "REGULAR (10 X 35G)",
-            cartpprice: "£60.00",
-        },
-        {
-            id: 3,
-            cartpimage: cartProductImg1,
-            cartpheader: "SOUR CREAM & ONION PRETZEL THINS",
-            cartpsize: "REGULAR (10 X 35G)",
-            cartpprice: "£50.00",
-        },
+        
     ];
     const updateProductNo = (event) => {
         const newValue = parseInt(event.target.value);
@@ -60,11 +49,13 @@ export const Cart = () => {
         <div>
             <button onClick={handleShow} href="#" className='accountIcon nav-link'><MdOutlineShoppingBag /></button>
             <OffCanvas show={showcart} placement='end' onHide={handleClose} name="end">
-                <OffCanvas.Header className='accountIcon'>
-                    <CgArrowLongLeft onClick={handleClose} className='cartClosebtn'/>
-                    <OffCanvas.Title>Your Cart</OffCanvas.Title>
+                <OffCanvas.Header className='accountIcon CartmainHeader'>
+                    <Link to='' onClick={handleClose} className='cartClosebtn'><img src={Backarrow} alt={Backarrow} /></Link>
+                    <OffCanvas.Title className="Cartheader">Your Cart</OffCanvas.Title>
                 </OffCanvas.Header>
                 <OffCanvas.Body>
+                    {cartProductData.length == 1 ? <p className="ProgressBartext"><img src={Star} alt={Star} /><span /> <strong>REWARDS</strong> Spend another £12.50 to qualify for free shipping</p> : cartProductData.length > 1 ? <p className="ProgressBartext"><img src={Star} alt={Star} /><span /> <strong>REWARDS</strong> Great! You have got free shipping!</p> : <p className="ProgressBartext"><img src={Star} alt={Star} /><span /> <strong>REWARDS</strong> Spend atleast £25.00 to qualify for free shipping</p>}
+                    {cartProductData.length == 1 ? <ProgressBar now={50} /> : cartProductData.length > 1 ? <ProgressBar now={100} /> : <ProgressBar now={0} />}
                     <div className="CartProduct">
                         {cartProductData.map((items, id) => {
                             return (
@@ -126,10 +117,16 @@ export const Cart = () => {
                                         </div>
                                     </div>
                                 </div>
-                                
+
                             );
                         })}
-                         <div className="Cartbtn"><button className="Checkoutbutton">Checkout</button></div>
+                        <div className="CartOrder-section w-100">
+                            <div className="CartOrder-details">
+                                <div className="CartOrder-header">Order Total</div>
+                                <div className="CartOrder-Price">£66.50</div>
+                            </div>
+                            <div className="Cartbtn"><button className="Checkoutbutton">Checkout</button></div>
+                        </div>
                     </div>
                 </OffCanvas.Body>
             </OffCanvas>
