55d281b2bfa47d0cf3c111ddd8ca3b1ae4f8db6a
nikhilsagar3108 <64469377+nikhilsagar3108@users.noreply.github.com>
2023-12-27 13:14:28 +0530

    Customer Review CHanges

--- client/src/Components/Shop/ViewProducts/CustomerReview/CustomerReview.jsx
+++ client/src/Components/Shop/ViewProducts/CustomerReview/CustomerReview.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './CustomerReview.css'
 import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from "react-icons/io";
 import ProgressBar from 'react-bootstrap/esm/ProgressBar';
+import Accordian from 'react-bootstrap/Accordion'
+import { useAccordionButton } from 'react-bootstrap/esm/AccordionButton';
+import AccordionBody from 'react-bootstrap/esm/AccordionBody';
+import Card from 'react-bootstrap/Card'
 export const CustomerReview = () => {
     return (
         <div>
@@ -15,7 +19,7 @@ export const CustomerReview = () => {
                                 <span><IoIosStar /></span> <span />
                                 <span><IoIosStar /></span> <span />
                                 <span><IoIosStar /></span> <span />
-                                <span><IoIosStarHalf/></span> <span />
+                                <span><IoIosStarHalf /></span> <span />
                             </div>
                             <p className="totalReviewText fw-bold">Based on 40 reviews</p>
                         </div>
@@ -28,7 +32,7 @@ export const CustomerReview = () => {
                                     <span><IoIosStar /></span> <span />
                                     <span><IoIosStar /></span> <span />
                                 </div>
-                                <div className='reviewBar'><ProgressBar now={88} className='reviewbar'/></div>
+                                <div className='reviewBar'><ProgressBar now={88} className='reviewbar' /></div>
                                 <div className="RSC-calculate">
                                     <span>88%</span> <span />
                                     <span>(35)</span>
@@ -42,7 +46,7 @@ export const CustomerReview = () => {
                                     <span><IoIosStar /></span> <span />
                                     <span><IoIosStarOutline /></span> <span />
                                 </div>
-                                <div className='reviewBar'><ProgressBar now={3} className='reviewbar'/></div>
+                                <div className='reviewBar'><ProgressBar now={3} className='reviewbar' /></div>
                                 <div className="RSC-calculate">
                                     <span>3%</span> <span />
                                     <span>(1)</span>
@@ -56,7 +60,7 @@ export const CustomerReview = () => {
                                     <span><IoIosStarOutline /></span> <span />
                                     <span><IoIosStarOutline /></span> <span />
                                 </div>
-                                <div className='reviewBar'><ProgressBar now={5} className='reviewbar'/></div>
+                                <div className='reviewBar'><ProgressBar now={5} className='reviewbar' /></div>
                                 <div className="RSC-calculate">
                                     <span>5%</span> <span />
                                     <span>(2)</span>
@@ -70,7 +74,7 @@ export const CustomerReview = () => {
                                     <span><IoIosStarOutline /></span> <span />
                                     <span><IoIosStarOutline /></span> <span />
                                 </div>
-                                <div className='reviewBar'><ProgressBar now={0} className='reviewbar'/></div>
+                                <div className='reviewBar'><ProgressBar now={0} className='reviewbar' /></div>
                                 <div className="RSC-calculate">
                                     <span>0%</span> <span />
                                     <span>(0)</span>
@@ -82,9 +86,9 @@ export const CustomerReview = () => {
                                     <span><IoIosStarOutline /></span> <span />
                                     <span><IoIosStarOutline /></span> <span />
                                     <span><IoIosStarOutline /></span> <span />
-                                    <span ><IoIosStarOutline  /></span> <span />
+                                    <span ><IoIosStarOutline /></span> <span />
                                 </div>
-                                <div className='reviewBar'><ProgressBar now={5} className='reviewbar'/></div>
+                                <div className='reviewBar'><ProgressBar now={5} className='reviewbar' /></div>
                                 <div className="RSC-calculate">
                                     <span>5%</span> <span />
                                     <span>(2)</span>
@@ -92,9 +96,38 @@ export const CustomerReview = () => {
                             </div>
                         </div>
                         <div className="writeReview">
-                            
+                            <button className='reviewaccordionbtn'>Write review</button>
                         </div>
                     </div>
+                    <Accordian className='my-5' defaultActiveKey="0">
+                        <Card>
+                            <Accordian.Collapse eventKey='0'>
+                                <Card.Body className='reviewForm'>
+                                    <form>
+                                        <div className='reviewInputs'>
+                                            <label htmlFor="name" className='reviewlable fw-bolder'>Name <span>(displayed publicly like 
+                                                <select name="namedisplayed" id="namedisplayed">
+                                                    <option value="John Smith">John Smith</option>
+                                                    <option value="John S.">John S.</option>
+                                                    <option value="J.S.">J.S.</option>
+                                                    <option value="Anonymous">Anonymous</option>
+                                                </select>)</span>
+                                            </label>
+                                            <input type="text" className='form-control mt-2 reviewInput' placeholder='Enter Your Name (public)' />
+                                        </div>
+                                        <div className='reviewInputs'>
+                                            <label htmlFor="email" className='reviewlable fw-bolder'>Email</label>
+                                            <input type="text" className='form-control mt-2 reviewInput' placeholder='Enter Your Email (private)' />
+                                        </div>
+                                        <div className='reviewInputs'>
+                                            <label htmlFor="rating" className='reviewlable fw-bolder'>Rating</label><br />
+                                            <button className='starbtn'><IoIosStar /></button>
+                                        </div>
+                                    </form>
+                                </Card.Body>
+                            </Accordian.Collapse>
+                        </Card>
+                    </Accordian>
                 </div>
             </div>
         </div>
