c4e616d2d3fa46547d7338e713955bc186cc1562
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-12-27 19:26:15 +0530

    make resposive to our story next section

--- client/src/Components/OurStory/MultipleComponents/ThemeThree/ThemeThree.jsx
+++ client/src/Components/OurStory/MultipleComponents/ThemeThree/ThemeThree.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React from "react";
 import "./ThemeThree.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const ThemeThree = ({ourStoryData, val, styles, direction}) => {
-  
-  if(!ourStoryData[`section_${val}_status`]){
-    return;
-  }
+const ThemeThree = ({ ourStoryData, val, styles, direction }) => {
+    if (!ourStoryData[`section_${val}_status`]) {
+        return;
+    }
 
-  return (
-    <>
-        <div className='theme-3' style={{...styles.fullBg, ...direction}}>
-            <div className="theme3-img">
+    return (
+        <>
+            <div className="theme-3" style={{ ...styles.fullBg, ...direction }}>
+                <div className="theme3-img">
                     <img src={ourStoryData[`section_${val}_img`]} alt="" />
+                </div>
+                <div className="theme-3-content">
+                    <h1 className="theme3-heading" style={styles.color}>
+                        {ourStoryData[`section_${val}_title`].en}
+                    </h1>
+                    <p className="theme3-desc" style={styles.color}>
+                        {" "}
+                        {ourStoryData[`section_${val}_description`].en}{" "}
+                    </p>
+                    {ourStoryData[`section_${val}_button_name`]?.en && (
+                        <Link
+                            to={ourStoryData[`section_${val}_button_link`].en}
+                            className="theme3-btn"
+                        >
+                            {ourStoryData[`section_${val}_button_name`].en}
+                        </Link>
+                    )}
+                </div>
             </div>
-            <div className='theme-3-content'>
-                <h1 className='theme3-heading' style={styles.color}>{ourStoryData[`section_${val}_title`].en}</h1>
-                <p className='theme3-desc' style={styles.color}> {ourStoryData[`section_${val}_description`].en} </p>
-                {ourStoryData[`section_${val}_button_name`]?.en && <Link to={ourStoryData[`section_${val}_button_link`].en} className='theme3-btn'>{ourStoryData[`section_${val}_button_name`].en}</Link>}
-            </div>
-        </div>
-    </>
-  )
-}
+        </>
+    );
+};
 
 export default ThemeThree;
--- client/src/Components/hooks/useWindowInnerWidth.js
+++ client/src/Components/hooks/useWindowInnerWidth.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
 const useWindowInnerWidth = () => {
-    const [widthValue,setWidthValue] = useState(window.innerWidth);
-    useEffect(()=>{
-        const handleWidth =()=>{
+    const [widthValue, setWidthValue] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleWidth = () => {
             setWidthValue(window.innerWidth);
         };
-        window.addEventListener('resize',handleWidth);
-        return()=>{
+        window.addEventListener('resize', handleWidth);
+        return () => {
 
-            window.removeEventListener('resize',handleWidth);
+            window.removeEventListener('resize', handleWidth);
         }
-    },[])
-  return widthValue;
+    }, [])
+    return widthValue;
 }
 export default useWindowInnerWidth;
