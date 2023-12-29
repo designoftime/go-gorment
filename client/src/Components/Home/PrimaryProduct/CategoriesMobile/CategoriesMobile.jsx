24fa33fb15d673b3be320fa1eb842285b915f79e
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-12-29 16:05:31 +0530

    make resposive to checkoout on page

--- client/src/Components/Home/PrimaryProduct/CategoriesMobile/CategoriesMobile.jsx
+++ client/src/Components/Home/PrimaryProduct/CategoriesMobile/CategoriesMobile.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import PretzelThings from '../images/Screenshot_PretzelThingsimg.avif'
-import Olives from '../images/Screenshot_Olivesimg.avif'
-import ChocoPretzel from '../images/Screenshot_ChocoPretzelimg.avif'
-import Bundles from '../images/Screenshot_Bundlesimg.avif'
-import { Link } from 'react-router-dom'
-import "../primaryProduct.css"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper/modules';
+import React from "react";
+import PretzelThings from "../images/Screenshot_PretzelThingsimg.avif";
+import Olives from "../images/Screenshot_Olivesimg.avif";
+import ChocoPretzel from "../images/Screenshot_ChocoPretzelimg.avif";
+import Bundles from "../images/Screenshot_Bundlesimg.avif";
+import { Link } from "react-router-dom";
+import "../primaryProduct.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const categoriesMobile = ({categoriesData}) => {
+const categoriesMobile = ({ categoriesData }) => {
     return (
-        <div className='main-primary-pro'>
-            <div className='PrimaryProduct-section my-5 '>
-                <div className='PPCmainComponent'>
+        <div className="main-primary-pro">
+            <div className="PrimaryProduct-section my-5 ">
+                <div className="PPCmainComponent">
                     <Swiper
-                        className='main-swiper-home'
+                        className="main-swiper-home"
                         spaceBetween={50}
                         slidesPerView={1}
                         pagination={{
@@ -27,39 +27,50 @@ const categoriesMobile = ({categoriesData}) => {
                         scrollbar={{ draggable: true }}
                         modules={[Pagination]}
                     >
-                        {
-                            categoriesData.map((eachCategory) => {
-                                let dynamicLink = eachCategory.name?.en.trim().split(" ").join("-"); 
-                                return (
-                                    <SwiperSlide key={eachCategory._id}>
-                                        <div className="primary-product-component mx-2 px-2">
-                                            <Link to={`/collections/all#${dynamicLink}`} className="primary-product-component-content">
-                                                <figure>
-                                                    <div className='imagediv'>
-
-                                                        <img className='img-fluid priproductimg' src={eachCategory.icon} />
-
-                                                    </div>
-                                                </figure>
-
+                        {categoriesData.map((eachCategory) => {
+                            let dynamicLink = eachCategory.name?.en
+                                .trim()
+                                .split(" ")
+                                .join("-");
+                            return (
+                                <SwiperSlide key={eachCategory._id}>
+                                    <div className="primary-product-component">
+                                        <Link
+                                            to={`/collections/all#${dynamicLink}`}
+                                            className="primary-product-component-content"
+                                        >
+                                            <figure>
+                                                <div className="imagediv">
+                                                    <img
+                                                        className="img-fluid priproductimg"
+                                                        src={eachCategory.cover}
+                                                    />
+                                                </div>
+                                            </figure>
+                                        </Link>
+                                        <div className="head-container">
+                                            <Link
+                                                to={`/collections/all#${dynamicLink}`}
+                                                className="ppcheader text-center text-decoration-none"
+                                            >
+                                                <h5 className="heading">
+                                                    {eachCategory.name.en}
+                                                </h5>
                                             </Link>
-                                            <div className="head-container">
-                                                <Link to={`/collections/all#${dynamicLink}`} className='ppcheader text-center text-decoration-none' >
-                                                    <h5 className="heading">{eachCategory.name.en}</h5>
-                                                </Link>
-                                                <p className="desc">{eachCategory.description.en}</p>
-                                            </div>
+                                            <p className="desc">
+                                                {eachCategory.description.en}
+                                            </p>
+                                            <Link> Shop Now </Link>
                                         </div>
-                                    <div> Shop Now </div>
-                                    </SwiperSlide>
-                                )
-                            })
-                        }
+                                    </div>
+                                </SwiperSlide>
+                            );
+                        })}
                     </Swiper>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default categoriesMobile
+export default categoriesMobile;
