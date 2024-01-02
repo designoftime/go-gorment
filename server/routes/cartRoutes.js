cf457485ad50ce55c4fcf0868e16ce13892880b7
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2024-01-02 18:54:15 +0530

    checkout delete cart and update orders

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addToCart, getCartByUserId, deleteCart, updateCartQuantity } = require("../controller/cartController");
+const { addToCart, getCartByUserId, deleteCart, updateCartQuantity, deleteAllCarts } = require("../controller/cartController");
 const router = express.Router();
 
 router.post("/add-to-cart", addToCart);
 router.get("/", getCartByUserId);
 router.put("/:id", updateCartQuantity);
 router.delete("/:id", deleteCart);
+router.delete("/delete-all", deleteAllCarts);
 
 module.exports = router;    
