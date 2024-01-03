3a578164c3164195904736221f2bf8f3a6927352
nikhilsagar3108 <64469377+nikhilsagar3108@users.noreply.github.com>
2024-01-03 15:36:19 +0530

    Invoice

--- client/src/Components/Invoice/Invoice.jsx
+++ client/src/Components/Invoice/Invoice.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Accounts/OrderHistory/OrderHistory.css'
 import { Table } from 'react-bootstrap'
 import { FaFileDownload } from "react-icons/fa";
 import { MdLocalPrintshop } from "react-icons/md";
-export const Invoice = (productInfo) => {
+import PretzelTHins from '../Shop/images/DarkChoc_360x.png'
+export const Invoice = ({productInfo}) => {
+  const [check,setCheck] = useState('checked')
+  console.log(productInfo,check);
   return (
       <div className="container mx-auto">
-      <div className="InvoiceHeader flex justify-between align-items-center">
+      <div className="InvoiceHeader d-flex justify-content-between align-items-center">
         <div className="IHeaderContent">
           <h3>Invoice ID: <span>165411154154</span></h3>
         </div>
@@ -16,14 +19,13 @@ export const Invoice = (productInfo) => {
         </div> 
       </div>
       <hr className='border-2'/>
-      <Table className='bg-white border-none dark:bg-gray-800 divide-y divide-gray-100 text-serif text-sm'>
-        <thead>
-          <th><h1 className='text-dark text-center font-[Snacksgifheaderfont] text-5xl text-pretty font-extrabold'><span className='p-2 rounded-circle bg-red-400 font-[Snacksgifheaderfont]'>GO</span> Gourmet</h1></th>
-        </thead>
-        <tbody>
-          <div className="InvoiceMain">
-            
-          </div>
+      <h1 className='text-dark text-center fw-boler'><span className='p-2 rounded-circle bg-danger'>GO</span> Gourmet</h1>
+      <Table className='bg-white border-none '>
+        <tbody className='InvoiceMain mx-auto'>
+          <tr className='mx-auto'>
+            <td className='OrderedProductimage'><img src={PretzelTHins} alt={PretzelTHins} /></td>
+            <td className='ProductName fs-5'>Dark-Choco Pretzel Thins</td>
+          </tr>
         </tbody>
       </Table>
       </div>
