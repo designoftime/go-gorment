84a7dcb48d8556af37d67eec7d23d861f8c413c9
nikhilsagar3108 <64469377+nikhilsagar3108@users.noreply.github.com>
2024-01-04 16:04:36 +0530

    checking

--- client/src/Components/Invoice/InvoiceDownload.jsx
+++ client/src/Components/Invoice/InvoiceDownload.jsx
@@ -1,10 +1,280 @@
 import React from 'react'
+import {
+  Document,
+  Font,
+  Image,
+  Page,
+  StyleSheet,
+  Text,
+  View,
+} from "@react-pdf/renderer";
+import { useTranslation } from "react-i18next";
 
 
-export const InvoiceDownload = () => {
+Font.register({
+  family: "Open Sans",
+  fonts: [
+    {
+      src: "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-regular.ttf",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-600.ttf",
+      fontWeight: 600,
+    },
+  ],
+});
+Font.register({
+  family: "DejaVu Sans",
+  fonts: [
+    {
+      src: "https://kendo.cdn.telerik.com/2017.2.621/styles/fonts/DejaVu/DejaVuSans.ttf",
+    },
+    {
+      src: "https://kendo.cdn.telerik.com/2017.2.621/styles/fonts/DejaVu/DejaVuSans-Bold.ttf",
+    },
+  ],
+});
+
+const styles = StyleSheet.create({
+  page: {
+    marginRight: 10,
+    marginBottom: 20,
+    marginLeft: 10,
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 29,
+    lineHeight: 1.5,
+  },
+  table: {
+    display: "table",
+    width: "auto",
+    color: "#4b5563",
+    marginRight: 10,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginTop: 0,
+    borderRadius: "8px",
+    borderColor: "#e9e9e9",
+    borderStyle: "solid",
+    borderWidth: 0.5,
+    padding: 0,
+    textAlign: "left",
+  },
+  tableRow: {
+    // margin: 'auto',
+    flexDirection: "row",
+    paddingBottom: 2,
+    paddingTop: 2,
+    textAlign: "left",
+    borderWidth: 0.8,
+    borderColor: "#E5E7EB",
+    borderBottom: "0",
+  },
+  tableRowHeder: {
+    // margin: 'auto',
+    flexDirection: "row",
+    backgroundColor: "#f9fafb",
+    paddingBottom: 4,
+    paddingTop: 4,
+    paddingLeft: 0,
+    borderBottomWidth: 0.8,
+    borderColor: "#E5E7EB",
+    borderStyle: "solid",
+    textTransform: "uppercase",
+    textAlign: "left",
+  },
+  tableCol: {
+    width: "25%",
+    textAlign: "left",
+
+    // borderStyle: 'solid',
+    // borderWidth: 1,
+    // borderLeftWidth: 0.5,
+    // borderTopWidth: 0.5,
+    // borderBottomWidth: 0.5,
+    // borderColor: '#d1d5db',
+  },
+  tableCell: {
+    margin: "auto",
+    marginTop: 5,
+    fontSize: 10,
+    // textAlign:'center',
+    paddingLeft: "0",
+    paddingRight: "0",
+    marginLeft: "13",
+    marginRight: "13",
+  },
+
+  tableCellQuantity: {
+    margin: "auto",
+    marginTop: 5,
+    fontSize: 10,
+    textAlign: "center",
+    paddingLeft: "0",
+    paddingRight: "0",
+    marginLeft: "12",
+    marginRight: "12",
+  },
+
+  invoiceFirst: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingBottom: 18,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderBottom: 1,
+    borderColor: "#f3f4f6",
+    // backgroundColor:'#EEF2FF',
+  },
+  invoiceSecond: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "baseline",
+    justifyContent: "space-between",
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 12,
+    marginLeft: "13",
+    marginRight: "13",
+  },
+  invoiceThird: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "baseline",
+    justifyContent: "space-between",
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderTop: 1,
+    borderColor: "#ffffff",
+    backgroundColor: "#f4f5f7",
+    borderRadius: 12,
+    marginLeft: "13",
+    marginRight: "13",
+
+    // backgroundColor:'#F2FCF9',
+  },
+  logo: {
+    width: 64,
+    height: 25,
+    bottom: 5,
+    right: 10,
+    marginBottom: 10,
+    textAlign: "right",
+    color: "#4b5563",
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    fontSize: 10.3,
+
+    marginRight: "39%",
+    textTransform: "uppercase",
+  },
+  title: {
+    color: "#2f3032",
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    fontSize: 8.1,
+    textTransform: "uppercase",
+  },
+  info: {
+    fontSize: 9,
+    color: "#6b7280",
+  },
+  infoCost: {
+    fontSize: 10,
+    color: "#6b7280",
+    marginLeft: "4%",
+    marginTop: "7px",
+    textAlign: "left",
+    width: "25%",
+  },
+  invoiceNum: {
+    fontSize: 9,
+    color: "#6b7280",
+    marginLeft: "6%",
+  },
+  topAddress: {
+    fontSize: 10,
+    color: "#6b7280",
+    width: "100%",
+    marginRight: "62%",
+    textAlign: "right",
+    whiteSapce: "nowrap",
+  },
+  amount: {
+    fontSize: 10,
+    color: "#ef4444",
+  },
+  totalAmount: {
+    fontSize: 10,
+    color: "#ef4444",
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "right",
+  },
+  status: {
+    color: "#10b981",
+  },
+  quantity: {
+    color: "#1f2937",
+    textAlign: "center",
+  },
+  itemPrice: {
+    color: "#1f2937",
+    textAlign: "left",
+  },
+  header: {
+    color: "#6b7280",
+    fontSize: 9,
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "left",
+  },
+
+  thanks: {
+    color: "#22c55e",
+  },
+  infoRight: {
+    textAlign: "right",
+    fontSize: 9,
+    color: "#6b7280",
+    width: "25%",
+    marginRight: "39%",
+    fontFamily: "Open Sans",
+  },
+  titleRight: {
+    textAlign: "right",
+    fontFamily: "Open Sans",
+    fontWeight: "bold",
+    fontSize: 8.1,
+    width: "25%",
+    marginRight: "39%",
+    textTransform: "uppercase",
+    color: "#2f3032",
+  },
+  topBg: {
+    // backgroundColor:'#EEF2FF',
+  },
+  invoiceDiv: {
+    alignItems: "baseline",
+  },
+});
+
+
+export const InvoiceDownload = ( {
+   
+  }) => {
+   
+  
   return (
-    <div>
-      
-    </div>
+    
+<div></div>
   )
 }
