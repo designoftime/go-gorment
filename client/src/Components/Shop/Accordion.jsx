9843553594754fd4e0dadfea2005876f80da0311
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2024-01-04 18:15:38 +0530

    fix slider of product page

--- client/src/Components/Shop/Accordion.jsx
+++ client/src/Components/Shop/Accordion.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import Collapse from 'react-collapse'
-import './ViewProducts/ViewProducts.css'
-import {IoMdClose} from 'react-icons/io'
+import React from "react";
+import Collapse from "react-collapse";
+import "./ViewProducts/ViewProducts.css";
+import { IoMdClose } from "react-icons/io";
 
-export const Accordion = ({ open, toggle, title, content}) => {
-
+export const Accordion = ({ open, toggle, title, content }) => {
     return (
-      
-        <div className="Accordian-total-content py-2  border-3">
+        <div className="Accordian-total-content border-3">
             <div
-                className= {`w-100 py-2 px-3 d-flex align-items-center  ${ open ? 'Collapseborder2':'Collapseborder'} justify-content-between`}
+                className={`w-100 px-5 py-2 d-flex align-items-center  ${
+                    open ? "Collapseborder2" : "Collapseborder"
+                } justify-content-between`}
                 onClick={toggle}
             >
-                <p className=" fw-bold accordion-title text-uppercase">
+                <p className="fw-bold accordion-title text-uppercase m-0 accordian-custom-font">
                     {title}
                 </p>
                 <div
                     className={
                         open
-                            ? "fs-5 fw-bolder"
-                            : "fs-5 fw-bolder rotate-45 collapsebutton"
+                            ? "fs-5 fw-bolder m-0"
+                            : "fs-5 fw-bolder rotate-45 collapsebutton m-0"
                     }
                 >
-                    <IoMdClose/>
+                    <IoMdClose />
                 </div>
             </div>
-            <Collapse isOpened={open} className='VPcollapse'>
-                <div className=" pb-3 accordion-content " dangerouslySetInnerHTML={{__html: content}}>
-                </div>
+            <Collapse isOpened={open} className="VPcollapse">
+                <div
+                    className=" pb-3 accordion-content "
+                    dangerouslySetInnerHTML={{ __html: content }}
+                ></div>
             </Collapse>
-
         </div>
-    )
-}
+    );
+};
