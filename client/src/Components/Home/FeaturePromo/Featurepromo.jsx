3082fb57f01f49c42eda6d772e4ccddb74c61faf
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2024-01-05 13:48:12 +0530

    fix bottom slider'

--- client/src/Components/Home/FeaturePromo/Featurepromo.jsx
+++ client/src/Components/Home/FeaturePromo/Featurepromo.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { featurePromo } from '../../../utils/Constants';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import useWindowInnerWidth from '../../hooks/useWindowInnerWidth';
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
 const FeaturePromo = ({featurePromoData}) => {
+
+  const showValue = useWindowInnerWidth();
 
   if(!featurePromoData){
     return;
@@ -11,17 +17,29 @@ const FeaturePromo = ({featurePromoData}) => {
     <div className="container-fluid g-0">
         <div className="Stockist-section container py-4  mx-auto">
             <div className="Stockist-main">
+            <Swiper
+              className='swiper'
+              spaceBetween={0}
+              slidesPerView={(showValue > 767) ? 5 : 1}
+              modules={(showValue > 767) ? [] : [Autoplay]}
+              loop={true}
+              autoplay={{
+                  delay: 1000,
+              }}>
             {
                 featurePromo.map((val,idx) => {
                     if(!featurePromoData[`feature_promo_${val}_img`]) return;
 
                     return (
-                        <div className="Stockist-content" key={idx}>
+                      <SwiperSlide key={idx}>
+                        <div className="Stockist-content">
                             <img src={featurePromoData[`feature_promo_${val}_img`]} alt="" />
                         </div>
+                      </SwiperSlide>
                     )
                 })
             }
+            </Swiper>
             </div>
         </div>
     </div>
